ef79634da9247d64daa71309a1945ab655c854be
unknown <mk@konitzer-training.de>
2021-10-19 12:38:52 +0000

    Test Delete Method

--- little-crm/src/app/features/customer/views/customer-list/customer-list.component.spec.ts
+++ little-crm/src/app/features/customer/views/customer-list/customer-list.component.spec.ts
@@ -4,29 +4,31 @@ import { CustomerListComponent } from './customer-list.component';
 import {RouterTestingModule} from "@angular/router/testing";
 import {CustomerService} from "../../services/customer.service";
 import {createCustomerServiceMock} from "../../../../../../mocks/customer/customerServiceMock";
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {customersMock} from "../../../../../../mocks/api/customers";
 
 fdescribe('CustomerListComponent', () => {
   let component: CustomerListComponent;
   let fixture: ComponentFixture<CustomerListComponent>;
 
-  const customerServiceMock = createCustomerServiceMock();
+  let customerServiceMock: any;
 
   beforeEach(async () => {
+    customerServiceMock = createCustomerServiceMock(customersMock);
+
     await TestBed.configureTestingModule({
       declarations: [ CustomerListComponent ],
       imports: [
         RouterTestingModule
       ],
       schemas: [
-        CUSTOM_ELEMENTS_SCHEMA
+        NO_ERRORS_SCHEMA
       ],
       providers: [
         {
           provide: CustomerService, useValue: customerServiceMock
         }
       ]
-
     })
     .compileComponents();
   });
@@ -40,4 +42,11 @@ fdescribe('CustomerListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a delete method', () => {
+    const id = customersMock[0].id
+    component.delete(id);
+    expect(customerServiceMock.deleteCustomer).toHaveBeenCalledWith(id);
+
+  })
 });
