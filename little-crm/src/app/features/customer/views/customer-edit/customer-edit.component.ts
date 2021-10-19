748b28460be84dccda56ea48a7c35db8ecf7d8d8
unknown <mk@konitzer-training.de>
2021-10-19 08:27:45 +0000

    Edit Validator

--- little-crm/src/app/features/customer/views/customer-edit/customer-edit.component.ts
+++ little-crm/src/app/features/customer/views/customer-edit/customer-edit.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {CustomerService} from "../../services/customer.service";
-import {FormBuilder} from "@angular/forms";
+import {AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 import {Customer} from "../../model/customer";
+
+function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? {
+      forbiddenName: {value: control.value}
+    } : null;
+  }
+}
 
 @Component({
   selector: 'crm-customer-edit',
@@ -13,8 +22,20 @@ export class CustomerEditComponent implements OnInit {
 
   public customerForm = this.fb.group({
     id: [],
-    name: [],
-    volume: []
+    name: ['',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        // Validators.pattern()
+        forbiddenNameValidator(/bob/i)
+      ]
+    ],
+    volume: [0,
+      [
+        Validators.required,
+        Validators.min(0)
+      ]
+    ]
   });
 
   constructor(
